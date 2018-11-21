d7c5a69f005d8c498edf88d5d61f262153a43d2e
angeLofdeath14 <43099603+angeLofdeath14@users.noreply.github.com>
2018-11-21 02:48:33 -0500

    Added player and movement horizontally and have camera follow it and adjusted the ground level

---
+++ HopHopRabbit/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[Header("Movement Settings")]
+    public float speed = 5.0f;
+	public float xMin, xMax, yMin, yMax;
+
+	private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Get input
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horiz, vert);
+
+        // Player movement code
+        rBody.velocity = movement * speed;
+
+		// Restrict our player movement
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, xMin, xMax), // Restricts X to xMin and xMax
+            Mathf.Clamp(rBody.position.y, yMin, yMax)); // Restrict Y to yMin and yMax
+	}
+}
